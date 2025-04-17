777aab1f370bf1db99de626cddf182db6260759c
Aditya-252003 <revankar.aditya817@gmail.com>
2025-04-17 16:52:47 +0530

    First initial process

---
+++ src/main/java/com/example/mtbs/MovieTickBookingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mtbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTickBookingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTickBookingSystemApiApplication.class, args);
+	}
+
+}
